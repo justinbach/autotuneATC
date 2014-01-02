0b877d9eb0f597ed2ce5d01de2975c858c8a913c
Justin Bachorik <jbachorik@npr.org>
2014-01-02 14:52:34 -0500

    not great, but it's something - modulating pitch according to note value

--- autotuner.rb
+++ autotuner.rb
@@ -14,30 +14,31 @@ require 'pathname'
 
 # a few settings
 TMP_DIR = '../tmp/'
-
+TMP_EXT = '.mp3'
+SUPPRESS_OUT = '>/dev/null 2>&1'
 
 # theme song data - keep it simple for now
 ATC = [
-	{ :pitch => "G4", 	:dur => 1},
-	{ :pitch => "C5", 	:dur => 1},
-	{ :pitch => "A5", 	:dur => 1},
-	{ :pitch => "F4", 	:dur => 1},
-	{ :pitch => "D4", 	:dur => 1},
-	{ :pitch => "G4", 	:dur => 1},
-	{ :pitch => "E4", 	:dur => 1},
-	{ :pitch => "C4", 	:dur => 5}, # should this be padded out as rest?
-	{ :pitch => "G4", 	:dur => 1},
-	{ :pitch => "C5", 	:dur => 1},
-	{ :pitch => "A5", 	:dur => 1},
-	{ :pitch => "F4", 	:dur => 1},
-	{ :pitch => "A5", 	:dur => 1},
-	{ :pitch => "B5", 	:dur => 1},
-	{ :pitch => "C5", 	:dur => 1}
+	{ :pitch => :G, 	:dur => 1, :oct => 4, :rel => 0},
+	{ :pitch => :C, 	:dur => 1, :oct => 5, :rel => 5},
+	{ :pitch => :A, 	:dur => 1, :oct => 5, :rel => 2},
+	{ :pitch => :F, 	:dur => 1, :oct => 4, :rel => -2},
+	{ :pitch => :D, 	:dur => 1, :oct => 4, :rel => -5},
+	{ :pitch => :G, 	:dur => 1, :oct => 4, :rel => 0},
+	{ :pitch => :E, 	:dur => 1, :oct => 4, :rel => -3},
+	{ :pitch => :C, 	:dur => 5, :oct => 4, :rel => -7}, # should this be padded out as rest?
+	{ :pitch => :G, 	:dur => 1, :oct => 4, :rel => 0},
+	{ :pitch => :C, 	:dur => 1, :oct => 5, :rel => 5},
+	{ :pitch => :A, 	:dur => 1, :oct => 5, :rel => 2},
+	{ :pitch => :F, 	:dur => 1, :oct => 4, :rel => -2},
+	{ :pitch => :A, 	:dur => 1, :oct => 5, :rel => 2},
+	{ :pitch => :B, 	:dur => 1, :oct => 5, :rel => 4},
+	{ :pitch => :C, 	:dur => 1, :oct => 5, :rel => 5}
 	]
 
 # check args
-if ARGV.length == 0 then
-	puts "Usage: autotuner.rb filename [duration_ratio]"
+if ARGV.length < 2 then
+	puts "Usage: autotuner.rb input_filename output_filename [duration_ratio]"
 	exit
 end
 
@@ -47,22 +48,31 @@ unless infile.exist?
 	puts "File not found: #{infile}"
 end
 
+outfile = ARGV[1]
+
 # ratio is second argument - dictates relative speed of notes
-duration_ratio = ARGV[1].to_i != 0 ? ARGV[1].to_i : 0.5
+duration_ratio = ARGV[2].to_i != 0 ? ARGV[2].to_i : 1
 
 # clear out tmp dir
-`rm #{TMP_DIR}*`
+`rm #{TMP_DIR}* #{SUPPRESS_OUT}`
 
 # split it into the number of source pieces dictated by the ATC theme song
 marker = 0 # track where we are in the audio file
 ATC.each_with_index do |note, i|
 	dur = duration_ratio * note[:dur]
-	`sox #{infile} #{TMP_DIR + i.to_s}.mp3 trim #{marker} #{dur}`
+	`sox #{infile} #{TMP_DIR + i.to_s + TMP_EXT} trim #{marker} #{dur} #{SUPPRESS_OUT}`
 	marker += dur
 end
 
 # autotune each piece
+0.upto(ATC.length - 1).each { |i| `sox #{TMP_DIR + i.to_s + TMP_EXT} #{TMP_DIR + i.to_s + TMP_EXT} pitch #{ATC[i][:rel] * 100}` }
 
 # put them together
-
 # and write it to a file
+
+merge_cmd = "sox "
+0.upto(ATC.length - 1).each { |i| merge_cmd += "#{TMP_DIR + i.to_s + TMP_EXT} "}
+merge_cmd += outfile
+`#{merge_cmd}`
+
+
