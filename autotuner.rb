010afb1daede966f4a7fffab3c860525c84be280
Justin Bachorik <jbachorik@npr.org>
2014-01-02 11:02:24 -0500

    splitting input file into correct number of constituent pieces

--- autotuner.rb
+++ autotuner.rb
@@ -9,38 +9,57 @@
 #
 ###############################################################################
 
+require 'pathname'
+
+
 # a few settings
-TMP_DIR = '../tmp'
+TMP_DIR = '../tmp/'
 
 
 # theme song data - keep it simple for now
 ATC = [
-	{ "pitch" => "G4", 	"dur" => 1},
-	{ "pitch" => "C5", 	"dur" => 1},
-	{ "pitch" => "A5", 	"dur" => 1},
-	{ "pitch" => "F4", 	"dur" => 1},
-	{ "pitch" => "D4", 	"dur" => 1},
-	{ "pitch" => "G4", 	"dur" => 1},
-	{ "pitch" => "E4", 	"dur" => 1},
-	{ "pitch" => "C4", 	"dur" => 5}, # should this be padded out as rest?
-	{ "pitch" => "G4", 	"dur" => 1},
-	{ "pitch" => "C5", 	"dur" => 1},
-	{ "pitch" => "A5", 	"dur" => 1},
-	{ "pitch" => "F4", 	"dur" => 1},
-	{ "pitch" => "A5", 	"dur" => 1},
-	{ "pitch" => "B5", 	"dur" => 1},
-	{ "pitch" => "C5", 	"dur" => 1}
+	{ :pitch => "G4", 	:dur => 1},
+	{ :pitch => "C5", 	:dur => 1},
+	{ :pitch => "A5", 	:dur => 1},
+	{ :pitch => "F4", 	:dur => 1},
+	{ :pitch => "D4", 	:dur => 1},
+	{ :pitch => "G4", 	:dur => 1},
+	{ :pitch => "E4", 	:dur => 1},
+	{ :pitch => "C4", 	:dur => 5}, # should this be padded out as rest?
+	{ :pitch => "G4", 	:dur => 1},
+	{ :pitch => "C5", 	:dur => 1},
+	{ :pitch => "A5", 	:dur => 1},
+	{ :pitch => "F4", 	:dur => 1},
+	{ :pitch => "A5", 	:dur => 1},
+	{ :pitch => "B5", 	:dur => 1},
+	{ :pitch => "C5", 	:dur => 1}
 	]
 
 # check args
 if ARGV.length == 0 then
-	puts "Usage: autotuner.rb filename"
+	puts "Usage: autotuner.rb filename [duration_ratio]"
 	exit
 end
 
 # first arg is input filename
+infile = Pathname.new(ARGV[0])
+unless infile.exist? 
+	puts "File not found: #{infile}"
+end
+
+# ratio is second argument - dictates relative speed of notes
+duration_ratio = ARGV[1].to_i != 0 ? ARGV[1].to_i : 0.5
+
+# clear out tmp dir
+`rm #{TMP_DIR}*`
 
 # split it into the number of source pieces dictated by the ATC theme song
+marker = 0 # track where we are in the audio file
+ATC.each_with_index do |note, i|
+	dur = duration_ratio * note[:dur]
+	`sox #{infile} #{TMP_DIR + i.to_s}.mp3 trim #{marker} #{dur}`
+	marker += dur
+end
 
 # autotune each piece
 
